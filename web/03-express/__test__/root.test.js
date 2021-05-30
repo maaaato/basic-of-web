0fa7792cad1c430fc375c687f59fdf031da8308e
maaaato <igosso.mato@gmail.com>
2021-05-30 14:27:44 +0900

    [Fix] package.json and test

--- web/03-express/__test__/root.test.js
+++ web/03-express/__test__/root.test.js
@@ -1,7 +1,3 @@
-const { json } = require('express')
-const express = require('express')
-const app = express()
-app.use(express.json())
 const {Root} = require('../root')
 
 test('status 200', () => {
