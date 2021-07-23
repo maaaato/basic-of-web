2dbb312d561e8dc83539907c4eb40b0a852472d3
maaaato <igosso.mato@gmail.com>
2021-07-23 14:59:15 +0900

    [Add] delete function

--- web/06-cors/myapp/public/javascripts/06.js
+++ web/06-cors/myapp/public/javascripts/06.js
@@ -1,21 +1,33 @@
 'use strict';
 const apiEndpoint = 'https://396bca642f2f.ngrok.io'
 const $ = (id) => document.getElementById(id);
-const xhr = new XMLHttpRequest();
+const uxhr = new XMLHttpRequest();
+const dxhr = new XMLHttpRequest();
+
+function deleteHistory(obj) {
+  dxhr.open('DELETE', apiEndpoint + '/history/' + obj.id);
+  dxhr.send();
+  document.getElementById(obj.id).remove();
+};
 
 function updateProfile() {
   const fd = new FormData($('edit-form'));
-  xhr.open('POST', apiEndpoint + '/update');
-  xhr.send(fd);
+  uxhr.open('POST', apiEndpoint + '/update');
+  uxhr.send(fd);
   removeForm();
 };
 
 function updateProfilePic() {
   const fd = new FormData();
   fd.append('profilePic', this.files[0])
-  xhr.open('POST', apiEndpoint + '/update');
-  xhr.send(fd);
+  uxhr.open('POST', apiEndpoint + '/update');
+  uxhr.send(fd);
   removeForm();
+};
+
+function fetchHistory() {
+  dxhr.open('GET', apiEndpoint + '/history');
+  dxhr.send();
 };
 
 function removeForm() {
@@ -30,8 +42,8 @@ function updateBackgroundImage(id, url) {
   target.style.backgroundImage = 'url(' + url + ')';
 };
 
-xhr.onreadystatechange = function() {
-      switch ( xhr.readyState ) {
+uxhr.onreadystatechange = function() {
+      switch ( uxhr.readyState ) {
           case 0:
               // 未初期化状態.
               console.log( 'uninitialized!' );
@@ -43,21 +55,57 @@ xhr.onreadystatechange = function() {
               console.log( 'loaded.' );
               break;
           case 3: // データ受信中.
-              console.log( 'interactive... '+xhr.responseText.length+' bytes.' );
+              console.log( 'interactive... '+uxhr.responseText.length+' bytes.' );
               break;
           case 4: // データ受信完了.
-              if( xhr.status == 200 || xhr.status == 304 ) {
-                  let profileCircle = document.getElementById('profile-circle');
-                  let data = JSON.parse(xhr.responseText);
-                  //- console.log( 'COMPLETE! :'+data.path );
-                  refreshProfile(data);
+              if( uxhr.status == 200 || uxhr.status == 304 ) {
+                  if (uxhr.responseText){
+                    let data = JSON.parse(uxhr.responseText);
+                    console.log( 'COMPLETE! :'+data);
+                    refreshProfile(data);
+                    fetchHistory();
+                  }
               } else {
-                  console.log( 'Failed. HttpStatus: '+xhr.statusText );
+                  console.log( 'Failed. HttpStatus: '+uxhr.statusText );
                   console.log(data);
               }
               break;
       }
   };
+
+dxhr.onreadystatechange = function() {
+    switch ( dxhr.readyState ) {
+        case 0:
+            // 未初期化状態.
+            console.log( 'uninitialized!' );
+            break;
+        case 1: // データ送信中.
+            console.log( 'loading...' );
+            break;
+        case 2: // 応答待ち.
+            console.log( 'loaded.' );
+            break;
+        case 3: // データ受信中.
+            console.log( 'interactive... '+dxhr.responseText.length+' bytes.' );
+            break;
+        case 4: // データ受信完了.
+            if( dxhr.status == 200 || dxhr.status == 304 ) {
+                if (dxhr.responseText){
+                    let data = JSON.parse(dxhr.responseText);
+                    console.log( 'COMPLETE!'+data.history);
+                    refreshHistory(data);
+                }
+            } if( dxhr.status == 204) {
+                if (dxhr.responseText){
+                    let data = JSON.parse(dxhr.responseText);
+                    console.log( 'COMPLETE!'+data.history);
+                }
+            } else {
+                console.log( 'Failed. HttpStatus: '+dxhr.statusText );
+            }
+            break;
+    }
+};
 
 function refreshProfile(data){
   if (data.imagePath){
@@ -72,6 +120,23 @@ function refreshProfile(data){
   }
 }
 
+function refreshHistory(data) {
+    let historyArea = document.getElementById('history-area');
+    while(historyArea.firstChild) {
+        historyArea.removeChild(historyArea.firstChild);
+    }
+    let e = "";
+    if (data){
+        data.forEach(function(v) {
+            console.log(v);
+
+            e += `
+            <div id='${v[0]}' onclick='deleteHistory(this)'>${v[1].date}: ${v[1].result}</div>
+            `
+        })
+        historyArea.innerHTML += e;
+    }
+}
 function editName(){
     removeForm();
     let editArea = document.getElementById('edit-area');
@@ -110,4 +175,5 @@ window.addEventListener('load', () => {
   $('profile-name').addEventListener('click', editName, false);
   $('profile-email').addEventListener('click', editEmail, false);
   $('profile-pic').addEventListener('change', updateProfilePic, false);
+  fetchHistory();
 });
--- web/06-cors/myapp/routes/api.js
+++ web/06-cors/myapp/routes/api.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const multer = require('multer');
-
+const uuid = require('uuid');
 const router = express.Router();
+
+// Setup for LRU Cache
+let LRU = require("lru-cache");
+const { json } = require('express');
+const lruOptions = {
+  max: 500, // maximum cache size
+  maxAge: 24 * 60 * 60 * 1000 // 1day
+}
+let cache = new LRU(lruOptions);
+const key = 'user_001';
+let lruvalue = {
+  history: new Map()
+}
+
+// Setup for multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    // upload to directory
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     extension = "jpg";
     switch(file.mimetype){
       case "image/png":
@@ -19,22 +34,59 @@ const storage = multer.diskStorage({
     cb(null, 'profile' + '.' + extension)
   }
 });
+
 const upload = multer({storage: storage});
 
 // Add Header
-router.use('/update', function(req, res, next){
+router.use('/', function(req, res, next){
+  // Allow Access-Control-Allow-Origin
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,HEAD,DELETE,OPTIONS');
   next();
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200).json({});
+function deleteHistory(v, id) {
+  v.history.delete(id)
+  cache.set(key, v)
+}
+
+function setCache(r) {
+  lruvalue.history.set(
+    uuid.v4(),
+    {
+      date: new Date(),
+      result: r
+    })
+  cache.set(key, lruvalue);
+}
+
+router.get('/history', function(req, res, next) {
+  try{
+    v = cache.get(key);
+    if (v){
+      res.status(200).json([...v.history]);
+    } else {
+      res.status(200).end();
+    }
+  } catch(e) {
+    console.log(e);
+  }
+});
+
+router.delete('/history/:uuid', function(req, res, next) {
+  try{
+    v = cache.get(key);
+    deleteHistory(v, req.params.uuid);
+    setCache('history delete: success!')
+    res.status(204).json([...cache.get(key).history]);
+  } catch(e) {
+    res.status(500);
+  }
 });
 
 router.post('/update', upload.single('profilePic'), function(req, res, next) {
-  console.log(req.file)
   try {
+    setCache('profile update: success!')
     res.json({
       "imagePath": (req.file) ? req.file.path : "",
       "name": (req.body.name) ? req.body.name : "",
@@ -42,15 +94,9 @@ router.post('/update', upload.single('profilePic'), function(req, res, next) {
     });
   } catch(e) {
     console.log(e);
-    console.log(req.body.filename);
-    res.status(500).json(e);
+    setCache('profile update: failed');
+    res.status(500);
   }
 });
 
 module.exports = router;
-
-
-
-
-
-
