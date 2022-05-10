31118ed1eba8cf3a02c367290296247aa013373e
maaaato <igosso.mato@gmail.com>
2022-05-10 22:15:00 +0900

    Add e2e

---
+++ web/13-e2e/my-app/cypress/integration/game_spec.js
@@ -0,0 +1,19 @@
+describe("The Game", () => {
+  it("Winner X", () => {
+    const v = [0, 3, 1, 4, 2];
+    cy.visit("/");
+    for (let i in v) {
+      cy.get("button").eq(v[i]).click();
+    }
+    cy.contains("Winner: X");
+  });
+
+  it("Winner ○", () => {
+    const v = [0, 3, 1, 4, 6, 5];
+    cy.visit("/");
+    for (let i in v) {
+      cy.get("button").eq(v[i]).click();
+    }
+    cy.contains("Winner: ○");
+  });
+});
---
+++ web/13-e2e/my-app/src/stories/Game.stories.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { Game } from '../components/Game';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Add/Game',
+  component: Game,
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Game {...args} />;
+
+export const Init = Template.bind({});
+Init.args = {
+  history: [{squares:Array(9).fill(null)}]
+};
+
+export const Draw = Template.bind({});
+const drawLines = [
+  {
+    squares: Array(9).fill(null),
+  },
+  {
+    squares: ["X",null,null,null,null,null,null,null,null]
+  },
+  {
+    squares: ["X","○",null,null,null,null,null,null,null]
+  },
+  {
+    squares: ["X","○","X",,null,null,null,null,null,null]
+  },
+  {
+    squares: ["X","○","X", null, null, "X",,null,null,null,null]
+  },
+  {
+    squares: ["X","○","X", null, null, "X", null, null, "○"]
+  },
+  {
+    squares: ["X","○","X", "X", null, "X", null, null, "○"]
+  },
+  {
+    squares: ["X","○","X", "X", null, "X", "○", null, "○"]
+  },
+  {
+    squares: ["X","○","X", "X", "○", "X", "○", null, "○"]
+  },
+  {
+    squares: ["X","○","X", "X", "○", "X", "○", "X", "○"]
+  },
+];
+
+Draw.args = {
+  history: drawLines
+};
